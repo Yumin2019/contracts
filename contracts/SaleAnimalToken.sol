e534e5527c7e19f73e672213c67014ad7b2a8f5a
Yumin Kim <yumingames@gmail.com>
2023-11-06 12:46:04 +0900

    feat: 토큰 구매 기능 추가

--- contracts/SaleAnimalToken.sol
+++ contracts/SaleAnimalToken.sol
@@ -24,4 +24,30 @@ contract SaleAnimalToken {
         animalTokenPrices[_animalTokenId] = _price;
         onSaleAnimalTokenArray.push(_animalTokenId);
     }
+
+    function purchaseAnimalToken(uint256 _animalTokenId) public payable {
+        uint256 price = animalTokenPrices[_animalTokenId];
+        address animalTokenOwner = mintAnimalTokenAddress.ownerOf(_animalTokenId);
+
+        require(price > 0, "Animal token not sale.");
+        require(price <= msg.value, "Caller sent lower than price");
+        require(animalTokenOwner != msg.sender, "Caller is animal token owner");
+
+        // 소유자는 돈을 받고, 구매자는 토큰을 얻는다. 
+        payable(animalTokenOwner).transfer(msg.value);
+        mintAnimalTokenAddress.safeTransferFrom(animalTokenOwner, msg.sender, _animalTokenId);
+
+        animalTokenPrices[_animalTokenId] = 0;
+        for(uint256 i = 0; i < onSaleAnimalTokenArray.length; ++i) {
+           if(animalTokenPrices[onSaleAnimalTokenArray[i]] == 0) {
+                onSaleAnimalTokenArray[i] = onSaleAnimalTokenArray[onSaleAnimalTokenArray.length - 1];
+                onSaleAnimalTokenArray.pop();
+                break;
+           }
+        }
+    }
+ 
+    function getOnSaleAnimalTokenArrayLength() view public returns (uint256) {
+        return onSaleAnimalTokenArray.length;
+    }
 }
